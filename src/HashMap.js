292e2f6458679c7b5a40853c80b53220bc85cdb9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-20 14:54:40 +0100

    HashMap.js: Created all methods

--- src/HashMap.js
+++ src/HashMap.js
@@ -2,7 +2,47 @@ class HashMap {
     constructor() {
         this.loadFactor = 0.8;
         this.capacity = new Array(16);
-    }
+    };
+
+    hash(key) {
+
+    };
+
+    set(key, value) {
+
+    };
+
+    get(key) {
+
+    };
+
+    has(key) {
+
+    };
+
+    remove(key) {
+
+    };
+
+    length() {
+
+    };
+
+    clear() {
+
+    };
+
+    keys() {
+
+    };
+
+    values() {
+
+    };
+
+    entries() {
+
+    };
 }
 
 export default HashMap;
