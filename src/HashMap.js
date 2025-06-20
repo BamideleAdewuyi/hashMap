3b5a6b642d7084c5ab90319c352cfe409dc22af3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-20 15:47:19 +0100

    HashMap.js: Wrote hash method

--- src/HashMap.js
+++ src/HashMap.js
@@ -5,7 +5,14 @@ class HashMap {
     };
 
     hash(key) {
+        let hashCode = 0;
 
+        const primeNumber = 19;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16
+        }
+
+        return hashCode;
     };
 
     set(key, value) {
